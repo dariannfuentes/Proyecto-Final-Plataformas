4efd7605f722f284aadded00c778b7fa61bceb5a
dariannfuentes <dariannfuentes@gmail.com>
2023-11-23 09:34:59 -0300

    Hice componentes ejercicios y rutinas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+
+interface Ejercicios{
+  nombre:string,
+  grupomuscular:string,
+  rangorep:string
+}
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +14,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  @ViewChild(IonModal)modal!: IonModal;
+  isToastOpen = false;
 
   constructor() {}
+  
 
+  ejercicios: Ejercicios[] = [
+    {nombre: 'Press de Banca',grupomuscular: 'Pecho', rangorep:'10-12'},
+    {nombre: 'Sentadillas',grupomuscular: 'Piernas', rangorep:'8-10'},
+    {nombre: 'Ropedown',grupomuscular: 'Triceps', rangorep:'8-10'},
+    {nombre: 'Curl de biceps',grupomuscular: 'Biceps', rangorep:'6-8'},
+    {nombre: 'Extensión de cuadriceps',grupomuscular: 'Piernas', rangorep:'4-6'},
+    {nombre: 'Jalon en polea', grupomuscular: 'Espalda', rangorep: '6-10'}
+  ];
+
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss('confirm');
+    this.isToastOpen = true
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
+interface Ejercicios{
+  nombre: string;
+}
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  @ViewChild(IonModal)modal!: IonModal;
+  isToastOpen = false;
 
   constructor() {}
 
+  rutinaPecho: Ejercicios[] = [
+    {nombre: 'Press de banca'},
+    {nombre: 'Press de banca inclinado'},
+    {nombre: 'Peck Deck'}
+  ]
+  rutinaEspalda: Ejercicios[] = [
+    {nombre: 'Jalon al pecho'},
+    {nombre: 'Remo Gironda'},
+    {nombre: 'Pull Ups'}
+  ]
+  rutinaPiernas: Ejercicios[] = [
+    {nombre: 'Sentadilla Hack'},
+    {nombre: 'Peso muerto'},
+    {nombre: 'Extension de cuadriceps'}
+  ]
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss('confirm');
+    this.isToastOpen = true;
+  }
+
+
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
 }
